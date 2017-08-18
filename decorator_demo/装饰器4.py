cd475509dbba56b3f6b9dcae7f471ead8f8795e8
李文周 <liwenzhou7@gmail.com>
2017-08-18 20:58:06 +0800

    update...

---
+++ decorator_demo/装饰器4.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Q1mi"
+# Date: 2017/8/12
+import time
+
+
+def foo(name):  # 我传的参数是一个函数名
+    # 定义了一个内部函数
+    def bar():
+        print(time.ctime(time.time()))  # 新功能
+        name()  # 函数名加()就相当于执行--> 我传进来原函数的函数名，我执行的就是原函数
+        print(time.ctime(time.time()))
+    return bar
+
+# 装饰器  --> 把原函数包了一层，然后给他还起原来的名
+
+# 装饰器的作用
+# 1. 不修改原函数的代码
+# 2。不改变调用方式
+# 3。 增加新功能  --> 打印当天时间
+
+
+# 定义一个原函数
+def f1():
+    print("hello world.")
+
+f1 = foo(f1)
+# 原函数的调用方式
+f1()
+
