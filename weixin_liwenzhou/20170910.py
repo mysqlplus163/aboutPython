25189cf39925c87d8f56ba438af279d930603c3e
Q1mi <liwenzhou7@gmail.com>
2017-09-10 23:53:16 +0800

    add weixin/OOP

---
+++ weixin_liwenzhou/20170910.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Q1mi"
+# Date: 2017/9/10
+
+"""
+面向对象
+"""
+
+
+class Hero(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def skill_1(self):
+        pass
+
+    def skill_2(self):
+        pass
+
+    def skill_3(self):
+        pass
+
+    def skill_4(self):
+        pass
